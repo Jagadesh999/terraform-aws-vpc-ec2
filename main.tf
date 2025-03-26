4c6f2aa41ebb644fc5700f862e1b3a697ee1a4c9
jagadesh <jagadesh1234@gmail.com>
2025-03-26 17:53:29 +0530

    Added Terraform AWS VPC and EC2 configuration

---
+++ main.tf
@@ -0,0 +1,84 @@
+provider "aws" {
+  region = "us-east-1"
+
+# Create a VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "terraform vpc"
+  }
+}
+
+# Create a Subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "MySubnet"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "my_gw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# Create a Route Table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# Add a Route to the Internet Gateway
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.my_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my_gw.id
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "subnet_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# Create a Security Group
+resource "aws_security_group" "my_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "MySecurityGroup"
+  }
+}
+
+# Create an EC2 Instance
+resource "aws_instance" "my_ec2" {
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  key_name               = "jagga.pem"  # Replace with your actual key
+
+  tags = {
+    Name = "jagadesh"
+  }
+}
+
